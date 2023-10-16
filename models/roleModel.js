f3fd341f2004054c8794328feccdc593379fa5ad
adnankhayrou <adnanekhayrou3@gmail.com>
2023-10-16 16:47:43 +0100

    adding user and role models

--- models/roleModel.js
+++ models/roleModel.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    
-})
+const mongoose = require('mongoose');
+
+const roleSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+});
+
+const Role = mongoose.model('Role', roleSchema);
+
+module.exports = Role;
