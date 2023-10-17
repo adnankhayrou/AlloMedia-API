041cca65db98528d25f2ad6d70b1d064a3c3bf92
adnankhayrou <adnanekhayrou3@gmail.com>
2023-10-17 21:01:47 +0100

    fixing mail message not sent

--- mailer/mailToUser.js
+++ mailer/mailToUser.js
@@ -5,15 +5,17 @@ const nodemailer = require("nodemailer");
 async function sendMailToUser(mailType){
     try {
         const transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            port: 587,
+            // host: 'smtp.ethereal.email',
+            // port: 587,
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
             auth: {
                 user: process.env.MAIL_FROM_ADDRESS,
                 pass: process.env.MAIL_PASSWORD
             }
         });
 
-        let details = await transporter.sendMailToUser(mailType);
+        let details = await transporter.sendMail(mailType) ;
         console.log("Message sent: %s", details.messageId);
 
     } catch (error) {
