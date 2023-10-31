51c371b3fcc751d9b65b70b6ec5b57dbe3d8bed8
adnankhayrou <adnanekhayrou3@gmail.com>
2023-10-31 16:49:43 +0100

    adding middleware to check user if logged or not

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController');
 const tokenMeddleware = require('../middlewares/token.meddleware');
+const isLogged = require('../middlewares/isLogged.meddleware');
 
 
 
-router.post('/register', authController.register);
+router.post('/register',isLogged, authController.register);
 router.get('/verification/:token', authController.verifyEmail);
-router.post('/login', authController.login);
+router.post('/login',isLogged, authController.login);
 router.get('/logout', authController.logout);
 router.post('/forgotpassword',authController.forgotPassword);
 router.post('/resetpassword/:token',tokenMeddleware, authController.resetPassword);
